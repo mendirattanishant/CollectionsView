16289c1af9f49c09f1b7388af404fdda2988684f
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2017-08-18 10:07:07 -0700

    Reading JSON file from main bundle and adding UICollectionsViewController delegate methods

--- Mercari/ViewController.swift
+++ Mercari/ViewController.swift
@@ -5,17 +5,124 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet private weak var collectionView: UICollectionView!
+class ViewController: UICollectionViewController {
+    
+    fileprivate let reuseIdentifier = "ItemCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
     fileprivate var contents: [[String: Any]] = []
-
+    fileprivate let itemsPerRow: CGFloat = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.a
+        loadData()
+        print(contents)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadData() {
+        if let path = Bundle.main.path(forResource: "all", ofType: "json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
+                do {
+                    let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    if let people : [NSDictionary] = jsonResult["data"] as? [NSDictionary] {
+                        for person: NSDictionary in people {
+                            for (name,value) in person {
+                                print("\(name) , \(value)")
+                                contents.append(person as! [String : Any])
+                            }
+                        }
+                    }
+                } catch {
+                    print("Error")
+                }
+            } catch {
+                print("Error")
+            }
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension ViewController {
+    //1
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return contents.count
+    }
+    
+    //2
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return contents[section].count
+    }
+    
+    //3
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //1
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath) as! ItemCell
+        //2
+        
+        cell.backgroundColor = UIColor.black
+        //3
+        //cell.imageView.image = flickrPhoto.thumbnail
+        
+        return cell
+    }
+}
+
+
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
+
+
+extension ViewController {
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                //self.imageView.image = UIImage(data: data)
+            }
+        }
     }
 }
